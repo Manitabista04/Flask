2cd68f30b8a9bb66146199dee9cd0ceb0d250f8e
Manita Bista <svm52@txstate.edu>
2026-03-15 22:54:18 -0500

    added images

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template('index.html')
-    #return "Function added successfully"
+    
     #return "<p>Hello, World!</p>"
 
 @app.route('/products')
