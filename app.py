54a1c3e0689b7a1449ff8ada0e6b1033a3d8b09a
Moon <svm52@txstate.edu>
2026-03-17 23:59:12 -0500

    added function

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
+class Todo(db.Model):
+    sno = db.Column(db.Integer , primary_key= True)
+    title = db.Column(db.String(200), nullable= False)
+    desc = db.Column(db.String(200), nullable= False)
+    date_created = db.Column(db.DateTime , default= datetime.utcnow)
+
+    def __repr__(self):
+        return f"{self.sno} - {self.title}"
+    
 @app.route("/")
 def hello_world():
     return render_template('index.html')
     
-    #return "<p>Hello, World!</p>"
 
 @app.route('/products')
 def products():
