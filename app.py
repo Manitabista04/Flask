971e5b099f7401f32d88e4236970d4115dda1c22
Moon <svm52@txstate.edu>
2026-03-19 11:41:58 -0500

    database

--- app.py
+++ app.py
@@ -9,14 +9,17 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     sno = db.Column(db.Integer , primary_key= True)
     title = db.Column(db.String(200), nullable= False)
-    desc = db.Column(db.String(200), nullable= False)
-    date_created = db.Column(db.DateTime , default= datetime.utcnow)
+    desc = db.Column(db.String(500), nullable= False)
+    date_created = db.Column(db.DateTime)
 
     def __repr__(self):
         return f"{self.sno} - {self.title}"
     
 @app.route("/")
 def hello_world():
+    todo = Todo(title = "First Todo" , desc = "Learn advance python")
+    db.session.add(todo)
+    db.session.commit()
     return render_template('index.html')
     
 
@@ -24,6 +27,8 @@ def hello_world():
 def products():
     return 'this is a products page'
 
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
